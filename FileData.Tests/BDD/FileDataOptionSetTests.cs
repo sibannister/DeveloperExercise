9e52be6708a8d320177c089b6f5f95583d3aea91
Simon Bannister <sibannister@yahoo.co.uk>
2018-09-09 19:47:02 +0100

    Single functionality names

---
+++ FileData.Tests/BDD/FileDataOptionSetTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using NDesk.Options;
+
+namespace FileData.Tests.BDD
+{
+
+    [Subject("matching abbreviated functionality version with single dash")]
+    public class when_receiving_v_with_a_single_dash
+    {
+        private static OptionSet optionsSet;
+        private static readonly string arg = "-v";
+        private static List<String> unmatched;
+        private Establish context = () => optionsSet = new FileDataOptionSet();
+
+        Because of = () => unmatched = optionsSet.Parse(new[] { arg });
+
+        It should_not_have_any_unmatched = () => unmatched.ShouldBeEmpty();
+    }
+
+    [Subject("matching abbreviated functionality version with double dash")]
+    public class when_receiving_v_with_a_double_dash
+    {
+        private static OptionSet optionsSet;
+        private static readonly string arg = "--v";
+        private static List<String> unmatched;
+        private Establish context = () => optionsSet = new FileDataOptionSet();
+
+        Because of = () => unmatched = optionsSet.Parse(new[] { arg });
+
+        It should_not_have_any_unmatched = () => unmatched.ShouldBeEmpty();
+    }
+
+    [Subject("matching abbreviated functionality version with forward slash")]
+    public class when_receiving_v_with_a_forward_slash
+    {
+        private static OptionSet optionsSet;
+        private static readonly string arg = "/v";
+        private static List<String> unmatched;
+        private Establish context = () => optionsSet = new FileDataOptionSet();
+
+        Because of = () => unmatched = optionsSet.Parse(new[] { arg });
+
+        It should_not_have_any_unmatched = () => unmatched.ShouldBeEmpty();
+    }
+
+    [Subject("matching abbreviated functionality size with single dash")]
+    public class when_receiving_s_with_a_single_dash
+    {
+        private static OptionSet optionsSet;
+        private static readonly string arg = "-s";
+        private static List<String> unmatched;
+        private Establish context = () => optionsSet = new FileDataOptionSet();
+
+        Because of = () => unmatched = optionsSet.Parse(new[] { arg });
+
+        It should_not_have_any_unmatched = () => unmatched.ShouldBeEmpty();
+    }
+
+    [Subject("matching abbreviated functionality size with double dash")]
+    public class when_receiving_s_with_a_double_dash
+    {
+        private static OptionSet optionsSet;
+        private static readonly string arg = "--s";
+        private static List<String> unmatched;
+        private Establish context = () => optionsSet = new FileDataOptionSet();
+
+        Because of = () => unmatched = optionsSet.Parse(new[] { arg });
+
+        It should_not_have_any_unmatched = () => unmatched.ShouldBeEmpty();
+    }
+
+    [Subject("matching abbreviated functionality size with forward slash")]
+    public class when_receiving_s_with_a_forward_slash
+    {
+        private static OptionSet optionsSet;
+        private static readonly string arg = "/s";
+        private static List<String> unmatched;
+        private Establish context = () => optionsSet = new FileDataOptionSet();
+
+        Because of = () => unmatched = optionsSet.Parse(new[] { arg });
+
+        It should_not_have_any_unmatched = () => unmatched.ShouldBeEmpty();
+    }
+}
---
+++ FileData/FileDataOptionSet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NDesk.Options;
+
+namespace FileData
+{
+    public class FileDataOptionSet : OptionSet
+    {
+        private bool _showVersion = false;
+        private bool _showSize = false;
+
+        public FileDataOptionSet()
+        {
+            Add("v", "Display Version of the file specified", v => _showVersion = true);
+            Add("s", "Display Size of the file specified", s => _showSize = true);
+        }
+    }
+}
